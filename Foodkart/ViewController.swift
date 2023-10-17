7423c24010293546a1858a516b0cf185461a13aa
Amardeep Bikkad <amardeep.bikkad@ril.com>
2023-10-17 14:02:33 +0530

    Initial Commit

---
+++ Foodkart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodkart
+//
+//  Created by Amardeep Bikkad on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
